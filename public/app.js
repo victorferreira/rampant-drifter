65b9aa41e063eac124732a253b28c205d46a0aa6
Victor Ferreira <victor.araujo.ferreira@gmail.com>
2016-09-22 10:39:14 -0300

    Updated at hyperdev.com

---
+++ public/app.js
@@ -0,0 +1,9 @@
+(function(){
+  console.log("Hello!");
+  // var url = "http://mobile-aceite.tcu.gov.br/mapa-da-saude/rest/estabelecimentos?municipio=Salvador&uf=BA&quantidade=5000";
+  // var xmlHttp = new XMLHttpRequest();
+  // xmlHttp.open("GET", url, false); // false for synchronous request
+  // xmlHttp.send();
+  // console.log(xmlHttp.responseText);
+  
+}());
